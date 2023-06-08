53790fbb358d36405b1cbde62ff305e2ed337409
TimAbuev <tim-r10@yandex.ru>
2023-06-08 18:32:03 +0300

    feat: aboutMe CSS

--- src/components/main/aboutMe/AboutMe.js
+++ src/components/main/aboutMe/AboutMe.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import photo from '../../../images/photo.jpg'
 
 function AboutMe() {
   return (
@@ -6,7 +7,7 @@ function AboutMe() {
       <h2 className="aboutMe__header-student">Студент</h2>
       <div className="aboutMe__decorate"></div>
       <div className="aboutMe__container">
-        <img className="aboutMe__avatar" alt="student's face"></img>
+        <img className="aboutMe__photo" alt="student's face" src={photo} />
         <article className="aboutMe__biography">
           <h3 className="aboutMe__header-name">Тимур</h3>
           <h4 className="aboutMe__profession">Фронтенд-разработчик, 29 лет</h4>
@@ -14,31 +15,32 @@ function AboutMe() {
             закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку,
             а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур».
             После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-          <Link to='https://github.com/TimAbuev'>Github</Link>
+          <Link className="aboutMe__link-github" to='https://github.com/TimAbuev'>Github</Link>
         </article>
         <article className="aboutMe__portfolio">
           <h4 className="aboutMe__header-portfolio">Портфолио</h4>
-          <ul className="aboutMe__list-portfolio">
-            <Link to='https://timabuev.github.io/how-to-learn'>
-              <li className="aboutMe__list-item">
-                Статичный сайт
-                <div className="aboutMe__list-icon"></div>
-              </li>
-            </Link>
-            <Link to='https://timabuev.github.io/russian-travel'>
-              <li className="aboutMe__list-item">
-                Адаптивный сайт
-                <div className="aboutMe__list-icon"></div>
-              </li>
-            </Link>
-            <Link to='https://timabuev.github.io/mesto'>
-              <li className="aboutMe__list-item">
-              Одностраничное приложение
-                <div className="aboutMe__list-icon"></div>
-              </li>
-            </Link>
 
-          </ul>
+          <div className="aboutMe__list-portfolio">
+            <Link className="aboutMe__link-of-list" to='https://timabuev.github.io/how-to-learn'>
+              <li className="aboutMe__list-item">
+                <p className="aboutMe__list-item-p">Статичный сайт</p>
+                <div className="aboutMe__list-icon"></div>
+              </li>
+            </Link>
+            <Link className="aboutMe__link-of-list" to='https://timabuev.github.io/russian-travel'>
+              <li className="aboutMe__list-item">
+                <p className="aboutMe__list-item-p">Адаптивный сайт</p>
+                <div className="aboutMe__list-icon"></div>
+              </li>
+            </Link>
+            <Link className="aboutMe__link-of-list" to='https://timabuev.github.io/mesto'>
+              <li className="aboutMe__list-item">
+                <p className="aboutMe__list-item-p">Одностраничное приложение</p>
+                <div className="aboutMe__list-icon"></div>
+              </li>
+            </Link>
+          </div>
+
         </article>
       </div>
     </section>
