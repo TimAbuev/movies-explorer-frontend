972d947bba3bff8e10683a46c041328ebe4c0ce6
TimAbuev <tim-r10@yandex.ru>
2023-06-09 14:29:53 +0300

    feat: promo & navTab tablet

--- src/components/main/promo/Promo.js
+++ src/components/main/promo/Promo.js
@@ -3,7 +3,7 @@ import NavTab from "../navTab/NavTab";
 function Promo() {
   return(
     <section className="promo">
-      <h1 className="promo__text">Учебный проект<br/> студента факультета<br/> Веб-разработки.</h1>
+      <h1 className="promo__text">Учебный проект студента факультета Веб-разработки.</h1>
       <div className="promo__test"></div>
       <NavTab></NavTab>
     </section>
