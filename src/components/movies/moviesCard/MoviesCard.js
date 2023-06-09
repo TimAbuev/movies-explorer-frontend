fe2bccd7ad0f9f20839b3e6a414384b80cdacdc4
TimAbuev <tim-r10@yandex.ru>
2023-06-09 21:22:54 +0300

    feat: MoviesCard mobile

--- src/components/movies/moviesCard/MoviesCard.js
+++ src/components/movies/moviesCard/MoviesCard.js
@@ -1,6 +1,18 @@
 function MoviesCard() {
   return (
-    <div className="moviesCard">CARD</div>
+    <div className="moviesCard">
+      <div className="moviesCard__text-container">
+        <p className="moviesCard__name">В погоне за Бенкси</p>
+        <p className="moviesCard__duration">27 минут</p>
+      </div>
+      <iframe className="moviesCard__iframe" src="https://www.youtube.com/embed/GVV06jTYjeY"
+        title="YouTube video player"
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen>
+      </iframe>
+      {/* <button className="moviesCard__button moviesCard__button_disabled">Сохранить</button> */}
+      <button className="moviesCard__button moviesCard__button_active"></button>
+    </div>
   );
 }
 export default MoviesCard;
