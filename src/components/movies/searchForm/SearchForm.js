8040b561afb3e1927ff23c7dc066326e20769c2a
TimAbuev <tim-r10@yandex.ru>
2023-06-17 00:02:10 +0300

    fix: validation to searchForm's input

--- src/components/movies/searchForm/SearchForm.js
+++ src/components/movies/searchForm/SearchForm.js
@@ -5,7 +5,7 @@ function SearchForm() {
     <section className="searchForm">
 
       <form className="searchForm__container-for-input">
-        <input className="searchForm__input" placeholder="Фильм" />
+        <input className="searchForm__input" placeholder="Фильм" minLength="2" maxLength="17" required/>
         <button className="searchForm__button-find" type="submit"></button>
         <div className="searchForm__decorate-vertical-line"></div>
         <div className="searchForm__decorate-loupe"></div>
