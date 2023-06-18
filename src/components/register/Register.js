5eccc176618fcbb99d9fa679a9932ada9fd3ca71
TimAbuev <tim-r10@yandex.ru>
2023-06-18 23:20:32 +0300

    fix: refactor the function Register

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -8,25 +8,24 @@ function Register(props) {
     handleRegisterSubmit,
   } = props;
 
-  const [name, setName] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const[formValue, setFormValue] = React.useState({
+    name: '',
+    email: '',
+    password: ''
+  });
 
-  function handleChangeName(e) {
-    setName(e.target.value);
-  }
-
-  function handleChangeEmail(e) {
-    setEmail(e.target.value);
-  }
-  function handleChangePassword(e) {
-    setPassword(e.target.value);
+  function handleOnChange(e) {
+    const {name, value} = e.target;
+    setFormValue({
+      ...formValue,
+      [name]: value
+    });
   }
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    handleRegisterSubmit(name, email, password);
+    handleRegisterSubmit(formValue.name, formValue.email, formValue.password);
   }
 
   return (
@@ -37,15 +36,15 @@ function Register(props) {
         <form className="register__form" name="register-form" onSubmit={handleSubmit}> 
           <fieldset className="register__fieldset">
             <label className="register__input-label" for="input-name">Имя</label>
-            <input className="register__input" name="name" id="input-name" type="text" onChange={handleChangeName} value={name}></input>
+            <input className="register__input" name="name" id="input-name" type="text" onChange={handleOnChange} value={formValue.name}></input>
             <span className="register__error"></span>
 
             <label className="register__input-label" for="input-email">E-mail</label>
-            <input className="register__input" name="email" id="input-email" type="email" onChange={handleChangeEmail} value={email}></input>
+            <input className="register__input" name="email" id="input-email" type="email" onChange={handleOnChange} value={formValue.email}></input>
             <span className="register__error"></span>
 
             <label className="register__input-label" for="input-password">Пароль</label>
-            <input className="register__input" name="password" id="input-password" type="password" onChange={handleChangePassword} value={password}></input>
+            <input className="register__input" name="password" id="input-password" type="password" onChange={handleOnChange} value={formValue.password}></input>
             <span className="register__error">Что-то пошло не так...</span>
           </fieldset>
           <button className="register__button" type="submit">Зарегистрироваться</button>
